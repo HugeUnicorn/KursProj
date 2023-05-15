9db5d793f4c77bd7a7b6bc5361f9d94314e56932
Асанов Михаил Александрович <10200276@college.local>
2023-05-15 13:28:28 +0300

    первый день

---
+++ KursProj/Model/AppData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursProj.Model
+{
+    public static class AppData
+    {
+        //https://www.youtube.com/watch?v=7SoudUnDk5k
+        public static kursachBDEntities db = new kursachBDEntities();
+
+    }
+}
---
+++ KursProj/Views/SignInPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using KursProj.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KursProj.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSighIn_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                var CurrentUser = AppData.db.User.FirstOrDefault(u => u.login == TBLogin.Text && u.password == TBPassword.Text);
+
+                if (CurrentUser == null)
+                {
+                    MessageBox.Show("Такого пользователя нет!", "Ошибка авторизации");
+                } else
+                {
+                    switch (CurrentUser.roleID)
+                    {
+                        case 1:MessageBox.Show("Здравствуйте, Администратор " + CurrentUser.name + "!");
+                            break;
+                        case 2:MessageBox.Show("Здравствуйте, гость " + CurrentUser.name + "!");
+                            break;
+                        default:MessageBox.Show("Ошибка");
+                            break;
+                    }
+                }
+            } catch (Exception ex) 
+            {
+                MessageBox.Show("Ошибка " + ex.Message.ToString());
+            }
+            
+        }
+    }
+}
